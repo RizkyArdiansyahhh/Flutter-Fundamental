ba07395334fc19d0eef31d3f562119665d79527d
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-02-03 11:02:12 +0700

    Image Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,8 +13,36 @@ class MainApp extends StatefulWidget {
   State<MainApp> createState() => _MainAppState();
 }
 
-class _MainAppState extends State<MainApp> {
+class _MainAppState extends State<MainApp> with WidgetsBindingObserver {
   int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    if (state == AppLifecycleState.inactive) {
+      print("Inactive");
+    }
+    if (state == AppLifecycleState.paused) {
+      print("Paused");
+    }
+    if (state == AppLifecycleState.resumed) {
+      print("Resumed");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,6 +57,16 @@ class _MainAppState extends State<MainApp> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(color: Colors.greenAccent),
+                    child: Image(
+                      image: NetworkImage(
+                          "https://tse1.mm.bing.net/th?id=OIP.vgiHJUdnoUMg4-SPk7AV-AHaKb&pid=Api&P=0&h=220"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                   Center(
                     child: Text(
                       counter.toString(),
